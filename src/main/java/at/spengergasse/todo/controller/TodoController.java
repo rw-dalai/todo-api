150b1b5b77f59bcc9ab3d0e2b0f54e415dc3c28f
wenz <wenz@spengergasse.at>
2025-10-16 18:40:54 +0200

    feat: create Todo i TodoController

--- src/main/java/at/spengergasse/todo/controller/TodoController.java
+++ src/main/java/at/spengergasse/todo/controller/TodoController.java
@@ -2,14 +2,15 @@ package at.spengergasse.todo.controller;
 
 import at.spengergasse.todo.model.Todo;
 import at.spengergasse.todo.persistence.TodoRepository;
+import at.spengergasse.todo.viewmodel.TodoRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 // Inversion of Control
 // Dependency Injection
@@ -30,6 +31,14 @@ import java.util.List;
 // DELETE      -> DELETE
 
 
+// HTTP Status Codes
+// 200 -> OK (GET)
+// 201 -> CREATED (POST)
+// 204 -> NO_CONTENT (DELETE)
+// 400 -> BAD_REQUEST
+// 404 -> NOT_FOUND
+
+
 // RESTful API
 // Representation State Transfer
 // Roy Fielding, PHD Dissertation, 2001
@@ -40,6 +49,11 @@ import java.util.List;
 // POST /api/todos           -> HTTP 201, save todo into DB
 // PUT /api/todos/{id}       -> HTTP 200, updates / replaces one existing todo by id
 // DELETE /api/todos/{id}    -> HTTP 204, delete todo by id
+
+// JSON Serialization (Jackson Object Mapper)
+// Object -> JSON Object
+
+// JSON -> Object (Jackson Object Mapper)
 
 // spring mvc
 @RestController
@@ -78,7 +92,37 @@ public class TodoController {
 
 
     @GetMapping("/{id}")
-    public void getOneTodo() {
+    public ResponseEntity<Todo> getOneTodo(@PathVariable Long id) {
 
+        // if found -> ResponseEntity (code=200, body=todo)
+        // else -> ResponseEntity (code=404)
+
+//        IMPERATIVE Version
+//        Optional<Todo> todoOptional = todoRepository.findById(id);
+//        if (todoOptional.isPresent()) {
+//            Todo todo = todoOptional.get();
+//            return ResponseEntity.ok(todo);
+//        } else {
+//            return ResponseEntity.notFound().build();
+//        }
+
+        // DECLARATIVE Version
+        return todoRepository
+            .findById(id)
+            .map(todo -> ResponseEntity.ok(todo))
+            .orElseGet( () -> ResponseEntity.notFound().build());
+    }
+
+
+
+    // { title: "Wäsche waschen" }
+    @PostMapping
+    public Todo createTodo(@RequestBody @Valid TodoRequest todoRequest) {
+
+        Todo todo = new Todo(todoRequest.title());
+
+        Todo savedTodo = todoRepository.save(todo);
+
+        return savedTodo;
     }
 }
--- src/main/java/at/spengergasse/todo/viewmodel/TodoRequest.java
+++ src/main/java/at/spengergasse/todo/viewmodel/TodoRequest.java
@@ -1,8 +1,15 @@
 package at.spengergasse.todo.viewmodel;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+// DTO
+// Data Transfer Object
+
+// immutable (nicht veränderbar)
+// getters, hashCode, equals, toString,
 
 public record TodoRequest(
-        @NotBlank(message = "Title is required")
+        @NotBlank(message = "title should not be blank")
         String title
 ) { }
